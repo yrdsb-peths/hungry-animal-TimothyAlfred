0ffe981dec623c7d6eb860440604750f1b413e7a
TimothyAlfred <101424271+TimothyAlfred@users.noreply.github.com>
2022-05-04 14:22:58 -0400

    Added pizza

--- Fish.java
+++ Fish.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class fish extends Actor
+public class Fish extends Actor
 {
     /**
      * Act - do whatever the fish wants to do. This method is called whenever
